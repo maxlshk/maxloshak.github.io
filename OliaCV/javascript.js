b4228f16942310438fb4f1132d80fd2dc040c9a7
mx_lshk <max.loshak.i@gmail.com>
2023-07-16 22:17:39 +0300

    updated 16.07 22:17

---
+++ OliaCV/javascript.js
@@ -0,0 +1,9 @@
+function changeColor(color) {
+    var elements = document.getElementsByClassName('body');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('blue', 'pink', 'yellow');
+        elements[i].classList.add(color);
+    }
+    var photo = document.getElementById('photo');
+    photo.src = 'assets/img/photo-'+color+'.jpg';
+}
