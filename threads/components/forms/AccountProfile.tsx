141898cca03a1787b9dc34c7708f6f0bb956f65b
mx_lshk <max.loshak.i@gmail.com>
2023-08-22 22:28:36 +0300

    comment posting

--- threads/components/forms/AccountProfile.tsx
+++ threads/components/forms/AccountProfile.tsx
@@ -18,6 +18,10 @@ import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod"
 import { ChangeEvent, useState } from "react"
 import { Textarea } from "../ui/textarea"
+import { isBase64Image } from "@/lib/utils"
+import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from "@/lib/actions/user.actions"
+import { usePathname, useRouter } from 'next/navigation'
 
 interface Props {
     user: {
@@ -33,7 +37,9 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
-
+    const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -50,12 +56,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
         const fileReader = new FileReader();
 
-        if(e.target.files && e.target.files.length > 0) {
+        if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
 
             setFiles(Array.from(e.target.files));
 
-            if(!file.type.includes("image")) return;
+            if (!file.type.includes("image")) return;
 
             fileReader.onload = async (event) => {
                 const imageDataUrl = event.target?.result?.toString() || '';
@@ -67,17 +73,42 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files)
+
+            if (imgRes && imgRes[0].url) {
+                values.profile_photo = imgRes[0].url;
+            }
+        }
+
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if (pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
+        }
+
     }
 
     return (
         <Form {...form}>
             <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="flex flex-col justify-start gap-10">
+                className="flex flex-col justify-start gap-10"
+            >
                 <FormField
                     control={form.control}
                     name="profile_photo"
@@ -112,6 +143,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                     onChange={(e) => handleImage(e, field.onChange)}
                                 />
                             </FormControl>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
@@ -131,6 +163,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
@@ -150,6 +183,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
@@ -169,6 +203,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                     {...field}
                                 />
                             </FormControl>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
