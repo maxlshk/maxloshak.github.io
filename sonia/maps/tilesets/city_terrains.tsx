45e2fae132326f7bd998ba29f73634bfbfc700e3
Maksym Loshak <max.loshak@newtonideas.com>
2026-05-18 22:25:40 +0300

    deploy A Map of Us

---
+++ sonia/maps/tilesets/city_terrains.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="city_terrains" tilewidth="16" tileheight="16" tilecount="6077" columns="59">
+ <image source="../../assets/exteriors/Modern_Exteriors_16x16/ME_Theme_Sorter_16x16/2_City_Terrains_16x16.png" width="944" height="1648"/>
+</tileset>
